8891ff9e5d127078005a2e79c17ae0a869c0345e
Florian Thoma <flori.thoma@gmail.com>
2021-11-21 22:10:59 +0100

    initial commit

---
+++ app/src/main/java/de/br/news/showcase/MainActivity.kt
@@ -0,0 +1,11 @@
+package de.br.news.showcase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
